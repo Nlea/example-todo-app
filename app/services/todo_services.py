5e8a47c58a00404772a9fdbfa5360321c49a9ce1
Nele Lea Uhlemann <nlea@Neles-MBP.home>
2023-12-04 13:03:54 +0100

    remove autometrics

--- app/services/todo_services.py
+++ app/services/todo_services.py
@@ -1,9 +1,8 @@
 from app.models.todo import Todo
 from fastapi import HTTPException
-from autometrics import autometrics
+
 from fastapi import HTTPException
-import time
-import random
+
 
 
 todos = [
@@ -11,23 +10,23 @@ todos = [
     Todo(id=2, item="Walk the dog"),
     # ... other todos ...
 ]
-@autometrics
+
 def get_todos_successful():
     return {
         "data": todos
     }
-@autometrics
+
 def get_todos_with_error():
     raise HTTPException(status_code=500, detail="Internal server error")
 
-@autometrics
+
 def add_todo(todo: Todo):
     #delay_seconds = random.uniform(1, 3) # Introcuding some latency in the function
     #time.sleep(delay_seconds)
     todos.append(todo)
     return todo
 
-@autometrics
+
 def update_todo(id: int, updated_todo: Todo)-> Todo:
      for todo in todos:
         if todo.id == id:
